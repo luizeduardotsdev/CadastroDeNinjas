fe453e9d1b002124af3d77ef5c5b2064e3a633e1
luiz <luizeduardotsdev@gmail.com>
2025-09-30 14:32:10 -0300

    feat: cria endpoints do controller de ninjas

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaController.java
@@ -1,16 +1,40 @@
 package dev.java10x.CadastroDeNinjas.ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninja")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Minha primeira mensagem nessa rota!";
     }
+
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarNinja() {
+        return "mostrar todos ninjas";
+    }
+
+    @GetMapping("/todosId")
+    public String mostrarNinjaPorId() {
+        return "mostrar ninja por id";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarNinjaPorId() {
+        return "alterar ninja alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarNinja() {
+        return "Ninja deletado por id";
+    }
+
 
 }
