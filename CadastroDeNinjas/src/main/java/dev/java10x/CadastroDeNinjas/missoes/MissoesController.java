17f361ebc68da1349cea1aba7b71d91861561cc8
luiz <luizeduardotsdev@gmail.com>
2025-09-30 14:49:25 -0300

    feat: cria endpoints do controller de missoes

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesController.java
@@ -1,15 +1,34 @@
 package dev.java10x.CadastroDeNinjas.missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
-
+@RequestMapping("/missoes")
 public class MissoesController {
-    @GetMapping("/missoes")
+
+    @GetMapping("/boasvindas")
     public String missoes() {
         return "aba de missoes";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "missao criada";
+    }
+
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "lista de missoes";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "missao alterada";
+    }
+
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "missao deletada";
     }
 }
