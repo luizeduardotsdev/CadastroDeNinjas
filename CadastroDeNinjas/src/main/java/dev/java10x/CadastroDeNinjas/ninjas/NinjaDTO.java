b67362cb916422c53f810a3390deb18b3542da5f
luiz <luizeduardotsdev@gmail.com>
2025-10-06 18:24:09 -0300

    chore: inclui a DTO e mapeia manualmente as entidades

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaDTO.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaDTO.java
@@ -1,4 +1,17 @@
 package dev.java10x.CadastroDeNinjas.ninjas;
 
+import dev.java10x.CadastroDeNinjas.missoes.MissoesModel;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class NinjaDTO {
+
+    private UUID id;
+    private String nome;
+    private int idade;
+    private String email;
+    private MissoesModel missoes;
+
 }
