3d1c1ead3913760fd4a95274770ce865b0d9907b
luiz <luizeduardotsdev@gmail.com>
2025-10-09 18:13:24 -0300

    feat: cria DTO e MAPPER e tira responsabilidade do service

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesDTO.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesDTO.java
@@ -1,4 +1,18 @@
 package dev.java10x.CadastroDeNinjas.missoes;
 
+import dev.java10x.CadastroDeNinjas.ninjas.NinjaModel;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class MissoesDTO {
+
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    private LocalDateTime startAt;
+    private List<NinjaModel> ninja;
 }
--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesMapper.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesMapper.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeNinjas.missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+    public MissoesModel map(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setStartAt(missoesDTO.getStartAt());
+        missoesModel.setNinja(missoesDTO.getNinja());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel){
+        MissoesDTO missoesDTO = new MissoesDTO();
+
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesDTO.getNome());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setStartAt(missoesModel.getStartAt());
+        missoesDTO.setNinja(missoesModel.getNinja());
+
+        return missoesDTO;
+    }
+
+
 }
